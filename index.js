41f5d21bc3e7540b7ac12f3366b95a1f413e5b83
Jason Gunawan Limantoro <jason@ewburgers.com>
2024-11-13 16:29:43 +0800

    enable tooltip only on desktop

--- index.js
+++ index.js
@@ -136,9 +136,15 @@ locationRelativePositions.forEach((position) => {
   container.appendChild(img);
 });
 
-const tooltipTriggerList = document.querySelectorAll(
-  '[data-bs-toggle="tooltip"]'
-);
-const tooltipList = [...tooltipTriggerList].map(
-  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
-);
+function isMobile() {
+  return window.innerWidth <= 768;
+}
+
+if (!isMobile()) {
+  const tooltipTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="tooltip"]'
+  );
+  tooltipTriggerList.forEach((tooltipTriggerEl) => {
+    new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+}
